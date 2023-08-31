c846e5a5695ee2e26e59e5edab1ed99e690f41ad
Paul Butler <57191996+butlerPaul1987@users.noreply.github.com>
2023-08-31 16:26:53 +0100

    Update pg_ctrl.sh

--- Bash/PostgreSQL CTRL/pg_ctrl.sh
+++ Bash/PostgreSQL CTRL/pg_ctrl.sh
@@ -5,21 +5,25 @@
 # Date:     19/07/2023 @ 16:48
 
 #@
-#@ USAGE: ./postgres-ctrl.sh ACTION SETTING [RESTOREFILE]
+#@ USAGE: 
+#@       ./postgres-ctrl.sh ACTION SETTING [RESTOREFILE]
 #@
-#@ BACKUP EXAMPLE: ./postgres-ctrl.sh --option backup --host cyient --port 5432
-#@ RESTORE EXAMPLE: ./postgres-ctrl.sh --option restore --host cyient --port 5432 --restore-file /var/backups/postgresql/backup.sql
+#@ BACKUP EXAMPLE: 
+#@       ./postgres-ctrl.sh --option backup --host cyient --port 5432
+#@ RESTORE EXAMPLE: 
+#@       ./postgres-ctrl.sh --option restore --host cyient --port 5432 --restore-file /var/backups/postgresql/backup.sql
 #@
 #@ ACTION
-#@   -h|--help                          Shows the help output
-#@   -o|--option                        i.e : backup, all-host-backup or restore
+#@   -h|--help                         Shows the help output
+#@   -o|--option                       i.e : backup, all-host-backup or restore
 #@
 #@ SETTING
-#@   -s |--host                         i.e : Cyient
-#@   -p |--port                         i.e: 5432 (default) or any port running
+#@   -s|--host                         i.e : Cyient
+#@   -p|--port                         i.e : 5432 (default) or any port running
+#@   -u|--user                         i.e : sysadmin etc
 #@
 #@ RESTOREFILE
-#@  -rf|--restore-file                  Set restore file i.e. /var/backups/postgresql/backup.sql
+#@  -rf|--restore-file                 Set restore file i.e. /var/backups/postgresql/backup.sql
 #@
 
 Usage(){
@@ -35,86 +39,60 @@ INFO='\033[1;33m'
 NC='\033[0m' # No Colour
 
 # Set vars up
-Init(){
-  SET_OPTION=
-  SET_ALLHOST=
-  SET_HOST=
-  SET_PORT=
-  SET_RESTORE_FILE=
-}
+SET_OPTION=
+SET_HOST=
+SET_ALLHOST=
+SET_PORT=
+SET_RESTORE_FILE=
+SET_USER=
 
 # Check for missing directory/ .pgpass file/...
-SysCheck(){
-  ! test -d "/var/backups/postgresql" && echo "Missing directory /var/backups/postgresql" && exit 1
-  ! test -f "/root/.pgpass" && echo "Missing .pgpass file - passwordless access won't work" && exit 1
-}
+! test -d "/var/backups/postgresql" && echo "Missing directory /var/backups/postgresql" && exit 1
+! test -f "/root/.pgpass" && echo "Missing .pgpass file - passwordless access won't work" && exit 1
+
+while [ ! -z "$1" ]; do
+  case $1 in
+  # SETTINGS =========
+    "-o"|"--option" )
+      shift; SET_OPTION=$1; SET="true"
+      ;;
+    "-u"|"--user" )
+      shift; SET_USER=$1; SET="true"
+      ;;
+  # HOST/PORT =========
+    "-s"|"--host" )
+      shift; SET_HOST=$1; SET="true"
+      ;;
+    "-as"|"--all-host" )
+      shift; SET_ALLHOST=$1; SET="true"
+      ;;
+    "-p"|"--port" )
+      shift; SET_PORT=$1; SET="true"
+      ;;
+  # RESTORE FILE
+    "-rf"|"--restore-file" )
+      shift; SET_RESTORE_FILE=$1; SET="true"
+      ;;
+  # HELP ===============
+    "-h"|"--help" )
+      Usage "Help" && exit 0;;
+    * ) Usage "Unknown argument '$1'" && exit 2;;
+  esac
+  shift
+done
+
 
 # Let's create some functions
-pgOption(){
-  # Check for options then perform action
-  DATETIME=$(date +%Y%m%j%H%M)
-  BACKFILE="/var/backups/postgresql/pgdump-${SET_HOST}-${DATETIME}.sql"
+DATETIME=$(date +%Y%m%j%H%M)
+BACKFILE="/var/backups/postgresql/pgdump-${SET_HOST}-${DATETIME}.sql"
+# Backup Process
+if [[ $SET_OPTION = "backup" ]]; then
+    pg_dumpall --host $SET_HOST --username $SET_USER --database 'postgres' --verbose --clean -f $BACKFILE
+# Restore Process:
+elif [[ $SET_OPTION = "restore" ]]; then
+    psql -U $SET_USER -h $SET_HOST -p $SET_PORT < $SET_RESTORE_FILE
 
-  # Backup Process
-  if [ $SET_OPTION = "backup" ]; then
-      pg_dumpall --host $SET_HOST --username 'sysbackup' --database 'postgres' --verbose --clean -f $BACKFILE
-
-  # All host Process:
-  # elif [ $SET_OPTION = "all-host-backup" ]; then
-  #    hosts = "server_1, 
-  #        server_2, 
-  #        server_3, 
-  #        server_4, 
-  #        server_5"
-  #    for h in $hosts; 
-  #      BACKFILE="/var/backups/postgresql/pgdump-${h}-${DATETIME}.sql"
-  #      do pg_dumpall --host $h --username 'sysbackup' --database 'postgres' --verbose --clean -f $BACKFILE; 
-  #    done
-
-  # Restore Process:
-  elif [ $SET_OPTION = "restore" ]; then
-      psql -U sysbackup -h $SET_HOST -p $SET_PORT < $SET_RESTORE_FILE
-
-  # else ask for valid selection
-  else
-      printf "Please select a valid option: ${INFO}[BACKUP]${NC}, ${INFO}[ALL-HOST-BACKUP]${NC} OR ${INFO}[RESTORE]${NC}\n"
-  fi
-
-progArgs(){
-  while [ ! -z "$1" ]; do
-    case $1 in
-    # SETTINGS =========
-      "-o"|"--option" )
-        shift; SET_OPTION=$1; SET="true"
-        ;;
-    # HOST/PORT =========
-      "-s"|"--host" )
-        shift; SET_HOST=$1; SET="true"
-        ;;
-      "-as"|"--all-host" )
-        shift; SET_ALLHOST=$1; SET="true"
-        ;;
-      "-p"|"--port" )
-        shift; SET_PORT=$1; SET="true"
-        ;;
-    # RESTORE FILE
-      "-rf"|"--restore-file" )
-        shift; SET_RESTORE_FILE=$1; SET="true"
-        ;;
-    # HELP ===============
-      "-h"|"--help" )
-        Usage "Help" && exit 0;;
-      * ) Usage "Unknown argument '$1'" && exit 2;;
-    esac
-    shift
-  done
-}
-
-# Main ------------------------
-Init \
-&& SysCheck \
-&& pgOption \
-&& progArgs $*
-
-
-exit $?
+# else ask for valid selection
+else
+    printf "Please select a valid option: ${INFO}[BACKUP]${NC}, ${INFO}[ALL-HOST-BACKUP]${NC} OR ${INFO}[RESTORE]${NC}\n"
+fi
