33c55c6d65f7393cc53c13aa96835ac2bdf9d5fc
Paul Butler <57191996+butlerPaul1987@users.noreply.github.com>
2023-10-15 20:50:34 +0100

    Update MicrosoftPoints.py

--- Python/Rewards For Two/MicrosoftPoints.py
+++ Python/Rewards For Two/MicrosoftPoints.py
@@ -4,42 +4,27 @@
 # Desc:     Written as a replacement to MicrosoftRewards.py as this utilised Selenium3
 #           which is no longer supported with msedge webdriver.
 
-# import modules
-# selenium
+# module imports
 from selenium import webdriver
 from selenium.webdriver.edge import service
 from selenium.webdriver.common.by import By
 import os
-
-# import random json list
 import random
 import requests
 import json
-
-# time/date based
 import datetime
 import time
 
-# config section #####################
-AccountEmail = [
-    "user1@email.com",
-    "user2@email.com"
-]
-AccountPword = [
-    "pass1",
-    "pass2"
-]
-RTime = [
-    1.4,2,2.1,2.2,1.9
-]
+# config section 
+AccountEmail = ["email1@email.com","email2@email.com"]
+AccountPword = ["pass1","pass2"]
+RTime = [1.4,2,2.1,2.2,1.9]
 List = 0
 StartPoints = []
 EndPoints = []
 today = datetime.date.today()
-# end config section #####################
 
 
-# script block -------------------------
 for user in AccountEmail:
     # add randomised search
     randomlists_url = "https://www.randomlists.com/data/words.json"
@@ -49,46 +34,40 @@ for user in AccountEmail:
     # Opens Edge webdriver
     edgeOption = webdriver.EdgeOptions()
     edgeOption.use_chromium = True
+    edgeOption.add_argument('log-level=2')      # 0: INFO / 1: WARNING / 2: LOG_ERROR / 3: LOG_FATAL #
     edgeOption.add_argument("start-maximized")
     edgeOption.binary_location = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
     s=service.Service(r'c:\users\paulf\Desktop\Drivers\msedgedriver.exe')
     driver = webdriver.Edge(service=s, options=edgeOption)
-
     driver.get("https://www.bing.com/search?q=steam")
     driver.maximize_window()
     time.sleep(5)
-    # driver.find_element_by_id("id_a").click() --deprecated method (use below)
     driver.find_element(By.ID,"id_a").click()
     time.sleep(5)
 
     # log on user
     print("Logging on using email:", user)
-    #driver.find_element_by_id("i0116").send_keys(user) --deprecated method (use below)
     driver.find_element(By.ID,"i0116").send_keys(user)
-    #driver.find_element_by_id("idSIButton9").click() --deprecated method (use below)
     driver.find_element(By.ID,"idSIButton9").click()
     time.sleep(5) 
 
     # enter password
-    #driver.find_element_by_id ("i0118").send_keys(AccountPword[List]) --deprecated method (use below)
     driver.find_element(By.ID,"i0118").send_keys(AccountPword[List])
-    #driver.find_element_by_id("idSIButton9").click() --deprecated method (use below)
     driver.find_element(By.ID,"idSIButton9").click()
     List = List + 1
     time.sleep(5)
 
     # click accept...
-    #driver.find_element_by_id("idSIButton9").click() --deprecated method (use below)
     driver.find_element(By.ID,"idSIButton9").click()
     time.sleep(5)
 
-    # search 30 times
+    # load bing (engine you need for this)
     search = 1
     driver.get("https://www.bing.com/search?q=qwerty")
     time.sleep(5)
-    #StartPoints = driver.find_element_by_id("id_rc").text --deprecated method (use below)
     StartPoints = driver.find_element(By.ID,"id_rc").text
-
+    
+    # search 30 times
     for word in words_list:
         GetRandTime = random.choice(RTime)
         url = f"https://www.bing.com/search?q={word}"
@@ -98,12 +77,37 @@ for user in AccountEmail:
         time.sleep(GetRandTime)
 
     # get deets of reward points count
-    #EndPoints = driver.find_element_by_id("id_rc").text --deprecated method (use below) 
     EndPoints = driver.find_element(By.ID,"id_rc").text
     Sum = int(EndPoints) - int(StartPoints)
     Output = f"[{today}]: '{user}' has {EndPoints} points [Up {Sum} points]"
 
     # output to file
+    f = open(("c:\\users\\paulf\\Desktop\\MSPoints\\Rewards.txt"), "a")
+    f.write(f"{Output}\n")
+    f.close()
+
+    # get daily sets :)
+    driver.get("https://rewards.bing.com/#daily-sets")
+    # select card 1
+    driver.find_element(By.XPATH,'//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[1]/div').click()
+    print(f"{user}: Completing daily set 1")
+    time.sleep(5)
+
+    # select card 2
+    driver.find_element(By.XPATH,'//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[2]/div').click()
+    print(f"{user}: Completing daily set 2")
+    time.sleep(5)
+
+    # select card 3
+    driver.find_element(By.XPATH,'//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[3]/div').click()
+    print(f"{user}: Completing daily set 3")
+    time.sleep(5)
+
+    # output data
+    setEndPoints = driver.find_element(By.XPATH, '//*[@id="balanceToolTipDiv"]/p/mee-rewards-counter-animation/span').text
+    setEndPoints = setEndPoints.replace(',','')
+    Sumation = int(setEndPoints) - int(EndPoints)
+    Output = f"[{today}]: '{user}' has [30/30] searches and completed [3/3] daily set points [Up {Sumation} points]"
     f = open(("c:\\users\\paulf\\Desktop\\MSPoints\\Rewards.txt"), "a")
     f.write(f"{Output}\n")
     f.close()
@@ -117,4 +121,3 @@ f = open(("c:\\users\\paulf\\Desktop\\MSPoints\\Rewards.txt"), "a")
 f.write("-===============================================================================-\n")
 f.close()
 exit()
-# end script block ---------------------
