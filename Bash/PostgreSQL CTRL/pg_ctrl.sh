47e0118ad36e57c97e6ce294a8bdc580332b3c55
Paul Butler <57191996+butlerPaul1987@users.noreply.github.com>
2024-01-18 16:40:22 +0000

    Update pg_ctrl.sh

--- Bash/PostgreSQL CTRL/pg_ctrl.sh
+++ Bash/PostgreSQL CTRL/pg_ctrl.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-
 # By:       PButler
 # Title:    postgres-ctrl.sh
 # Date:     19/07/2023 @ 16:48
 # Version:  v1.0 - Initial Build
+
+#   pgpass example format:
+#   hostname:port:database:username:password
+#   notes: hostname must match exactly...
 
 #@
 #@ USAGE:
 #@       ./postgres-ctrl.sh ACTION SETTING [RESTOREFILE]
 #@
 #@ BACKUP EXAMPLE:
-#@       ./postgres-ctrl.sh --option backup --host cyient --port 5432
+#@       ./postgres-ctrl.sh --option backup --host hostname --port 5432
+#@ CRON EXAMPLE:
+#@       ./postgres-ctrl.sh --option cron 
 #@ RESTORE EXAMPLE:
-#@       ./postgres-ctrl.sh --option restore --host cyient --port 5432 --restore-file /var/backups/postgresql/backup.sql
+#@       ./postgres-ctrl.sh --option restore --host hostname --port 5432 --restore-file /var/backups/postgresql/backup.sql.bz2
 #@
 #@ ACTION
 #@   -h|--help                         Shows the help output
-#@   -o|--option                       i.e : backup, all-host-backup or restore
+#@   -o|--option                       i.e : backup, restore or cron
 #@
 #@ SETTING
-#@   -s|--host                         i.e : Cyient
+#@   -s|--host                         i.e : hostname
 #@   -p|--port                         i.e : 5432 (default) or any port running
 #@   -u|--user                         i.e : sysadmin etc
 #@
@@ -27,35 +32,7 @@
 #@  -rf|--restore-file                 Set restore file i.e. /var/backups/postgresql/backup.sql
 #@
 #@
-#@ LOG FIE GENERATED:                 /var/backups/postgresql/${HOST}-${DATETIME}.log
-
-# - printf colour sets
-WARNING='\033[1;31m'
-SUCCESS='\033[1;32m'
-INFO='\033[1;33m'
-
-# banner colours
-LCYAN='\033[1;36m'  # Cyan
-MAGEN='\033[1;35m'  # Magenta
-NC='\033[0m'        # No Colour
-
-# functions
-Usage(){
-    cat $0 | grep '^#@' | sed -e 's/^#@//g'
-    echo -e "$*\n"
-    return 0
-}
-
-banner(){
-    printf "
-    ${LCYAN}██████╗ ${MAGEN} ██████╗       ${LCYAN} ██████╗████████╗██████╗ ██╗
-    ${LCYAN}██╔══██╗${MAGEN}██╔════╝       ${LCYAN}██╔════╝╚══██╔══╝██╔══██╗██║
-    ${LCYAN}██████╔╝${MAGEN}██║  ███╗█████╗${LCYAN}██║        ██║   ██████╔╝██║
-    ${LCYAN}██╔═══╝ ${MAGEN}██║   ██║╚════╝${LCYAN}██║        ██║   ██╔══██╗██║
-    ${LCYAN}██║     ${MAGEN}╚██████╔╝      ${LCYAN}╚██████╗   ██║   ██║  ██║███████╗
-    ${LCYAN}╚═╝     ${MAGEN} ╚═════╝       ${LCYAN} ╚═════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝${NC}
-    \n"
-}
+#@ LOG FIE GENERATED:                  /var/backups/postgresql/${HOST}-${DATETIME}.log
 
 # Set vars up
 SET_OPTION=
@@ -64,10 +41,6 @@ SET_ALLHOST=
 SET_PORT=
 SET_RESTORE_FILE=
 SET_USER=
-
-# Check for missing directory / .pgpass file
-! test -d "/var/backups/postgresql" && banner && echo "Missing directory /var/backups/postgresql - please create" && exit 1
-! test -f "/root/.pgpass" && banner && echo "Missing .pgpass file - passwordless access will not work" && exit 1
 
 while [ ! -z "$1" ]; do
   case $1 in
@@ -100,57 +73,90 @@ while [ ! -z "$1" ]; do
   shift
 done
 
+# Log colour sets
+WARNING='\033[1;31m'  # Red
+SUCCESS='\033[1;32m'  # Green
+INFO='\033[1;33m'     # Yellow
 
-# Let's create some functions
+# Banner colour sets
+LCYAN='\033[1;36m'    # Cyan
+MAGEN='\033[1;35m'    # Magenta
+NC='\033[0m'          # No Colour
+
+# functions
+Usage(){
+    cat $0 | grep '^#@' | sed -e 's/^#@//g'
+    echo -e "$*\n"
+    return 0
+}
+
+LogWrite(){
+    DATETIME=$(date '+%Y%m%d')
+    FILE="/var/backups/postgresql/${SET_HOST}-${DATETIME}.log"
+    printf "$(date '+%b %d %T') : $1\n" >> $FILE;
+}
+
+banner(){
+    printf "
+    ${LCYAN}██████╗ ${MAGEN} ██████╗       ${LCYAN} ██████╗████████╗██████╗ ██╗
+    ${LCYAN}██╔══██╗${MAGEN}██╔════╝       ${LCYAN}██╔════╝╚══██╔══╝██╔══██╗██║
+    ${LCYAN}██████╔╝${MAGEN}██║  ███╗█████╗${LCYAN}██║        ██║   ██████╔╝██║
+    ${LCYAN}██╔═══╝ ${MAGEN}██║   ██║╚════╝${LCYAN}██║        ██║   ██╔══██╗██║
+    ${LCYAN}██║     ${MAGEN}╚██████╔╝      ${LCYAN}╚██████╗   ██║   ██║  ██║███████╗
+    ${LCYAN}╚═╝     ${MAGEN} ╚═════╝       ${LCYAN} ╚═════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝${NC}
+    \n"
+}
+
+# Check for missing directory / .pgpass file
+! test -d "/var/backups/postgresql" && banner && echo "Missing directory /var/backups/postgresql - please create" && exit 1
+! test -f "/root/.pgpass" && banner && echo "Missing .pgpass file - passwordless access will not work" && exit 1
+
 banner
 printf "[${INFO}-${NC}]: Adding data into variables\n"
-DATETIME=$(date +%Y%m%j%H%M)
+DATETIME=$(date +%Y%m%d%H%M)
 BACKFILE="/var/backups/postgresql/pgdump-${SET_HOST}-${DATETIME}.sql"
-FILE="/var/backups/postgresql/${SET_HOST}-${DATETIME}.log"
 printf "[${SUCCESS}+${NC}]: Complete\n"
 
-# Backup Process
+##################
+# Backup Process #
+##################
 if [[ $SET_OPTION = "backup" ]]; then
     printf "[${INFO}-${NC}]: Creating backup\n"
-    LOGTIME=$(date '+%b %d %T')
-    echo "$LOGTIME: PGDUMP -> $BACKFILE" > $FILE
+    LogWrite "PGDUMP -> $BACKFILE"
     /usr/bin/pg_dumpall --host $SET_HOST --username $SET_USER --database 'postgres' --verbose --clean -f $BACKFILE
     # Perform check
     RESULT=$?
     if [ $RESULT -eq 0 ]; then
       # session variables
-      LOGTIME=$(date '+%b %d %T')
       SIZE=$(ls -al $BACKFILE | awk '{print $5}')
-      echo "$LOGTIME: OK - PGDUMP -> $BACKFILE Size=$SIZE" >> $FILE
-      
+      LogWrite "OK - PGDUMP -> $BACKFILE Size=$SIZE"
       printf "[${SUCCESS}+${NC}]: Complete\n"
+
+      # Compress the backup
+      printf "[${INFO}-${NC}]: Compressing backup\n"
+      LogWrite "Archiving $BACKFILE ..."
+
+      /bin/bzip2 $BACKFILE  # this can be slow
+      RESULT=$?
+      if [ $RESULT -eq 0 ]; then
+        NEWBACKFILE="$BACKFILE.bz2"
+        SIZE=$(ls -al $NEWBACKFILE | awk '{print $5}')
+        LogWrite "\tOK - Created -> $NEWBACKFILE" 
+        LogWrite "\tSize=$SIZE"
+        printf "[${SUCCESS}+${NC}]: Complete\n"
+      else
+        LogWrite "FAIL - PGDUMP Failed!"
+        printf "[${WARNING}@${NC}]: Failed\n"
+      fi
     else
-      LOGTIME=$(date '+%b %d %T')
       SIZE=$(ls -al $BACKFILE | awk '{print $5}')
-      echo "$LOGTIME: FAIL - PGDUMP -> $BACKFILE Size=" >> $FILE
+      LogWrite "FAIL - PGDUMP -> $BACKFILE Size="
       printf "[${WARNING}@${NC}]: Failed\n"
     fi
 
-    # Compress the backup
-    printf "[${INFO}-${NC}]: Compressing backup\n"
-    LOGTIME=$(date '+%b %d %T')
-    echo "$LOGTIME: Archiving $BACKFILE ..." >> $FILE
-
-    /bin/bzip2 -v $BACKFILE  # this can be slow
-    RESULT=$?
-    if [ $RESULT -eq 0 ]; then
-      LOGTIME=$(date '+%b %d %T')
-      NEWBACKFILE="$BACKFILE.bz2"
-      SIZE=$(ls -al $NEWBACKFILE | awk '{print $5}')
-      echo "$LOGTIME: OK - Created -> $NEWBACKFILE Size=$SIZE" >> $FILE
-      printf "[${SUCCESS}+${NC}]: Complete\n"
-    else
-      LOGTIME=$(date '+%b %d %T')
-      echo "$LOGTIME: FAIL - PGDUMP Failed!" >> $FILE
-      printf "[${WARNING}@${NC}]: Failed\n"
-    fi
-
-# Restore Process:
+###################
+# Restore Process #
+###################
 elif [[ $SET_OPTION = "restore" ]]; then
     printf "[${INFO}-${NC}]: Creating restore with file: $SET_RESTORE_FILE\n"
     /bin/bzip2 -dkf $SET_RESTORE_FILE
@@ -170,6 +176,52 @@ elif [[ $SET_OPTION = "restore" ]]; then
     else
       printf "[${WARNING}@${NC}]: Failed\n"
     fi
+
+################
+# Cron Process #
+################
+elif [[ $SET_OPTION = "cron" ]]; then
+    printf "[${INFO}-${NC}]: Creating backup\n"
+    HOSTLIST="host 1 host 2 host 3 etc etc"
+    printf "Host list: $HOSTLIST\n"
+    for SET_HOST in $HOSTLIST; do
+      printf "Backing up.. '$SET_HOST'\n"
+
+      # cron variables
+      BACKUPFILE="/var/backups/postgresql/pgdump-${SET_HOST}-${DATETIME}.sql"
+      printf "Creating file.. '$BACKUPFILE'\n"
+      USER=$(cat ~/.pgpass | grep $SET_HOST | awk -F : {'print $4'})
+
+      LogWrite "PGDUMP -> $BACKFILE"
+      /usr/bin/pg_dumpall --host $SET_HOST --username $USER --database 'postgres' --verbose --exclude-database='azure_*' --clean -f $BACKUPFILE
+      # Perform check
+      RESULT=$?
+      if [ $RESULT -eq 0 ]; then
+        # session variables
+        SIZE=$(ls -al $BACKUPFILE | awk '{print $5}')
+        LogWrite "\tOK - PGDUMP -> $BACKUPFILE Size=$SIZE"
+        printf "[${SUCCESS}+${NC}]: Complete\n"
+
+        # Compress the backup
+        printf "[${INFO}-${NC}]: Compressing backup\n"
+        LogWrite "Archiving $BACKUPFILE ..."
+        /bin/bzip2 $BACKUPFILE  # this can be slow
+        RESULT=$?
+        if [ $RESULT -eq 0 ]; then
+          NEWBACKFILE="$BACKUPFILE.bz2"
+          SIZE=$(ls -al $NEWBACKFILE | awk '{print $5}')
+          LogWrite "\tOK - Created -> $NEWBACKFILE Size=$SIZE"
+          printf "[${SUCCESS}+${NC}]: Complete\n"
+        else
+          LogWrite "FAIL - Compression Failed!"
+          printf "[${WARNING}@${NC}]: Failed\n"
+        fi
+      else
+        SIZE=$(ls -al $BACKUPFILE | awk '{print $5}')
+        LogWrite "FAIL - PGDUMP -> $BACKUPFILE Size="
+        printf "[${WARNING}@${NC}]: Failed\n"
+      fi
+    done
 
 # else ask for valid selection
 else
